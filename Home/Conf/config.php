8a19c76ca59889966ba5e65e47e06efb4d3ae070
Bin Zhang <30135878@qq.com>
2017-09-20 13:31:16 +0800

    修改数据库。

--- Home/Conf/config.php
+++ Home/Conf/config.php
@@ -2,15 +2,15 @@
 return array(
     //'配置项'=>'配置值'
     'DB_TYPE'   => 'mysql', // 数据库类型
-    'DB_HOST'   => 'localhost', // 服务器地址
-    'DB_NAME'   => 'valve', // 数据库名
+    'DB_HOST'   => 'rm-bp108sczjo3c1vtk9o.mysql.rds.aliyuncs.com', // 服务器地址
+    'DB_NAME'   => 'valve_prod', // 数据库名
     'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => 'Dsh12345', // 密码
+    'DB_PWD'    => 'Wgbzoombin123(*&', // 密码
     'DB_PORT'   => 3306, // 端口
-    'DB_PREFIX' => '', // 数据库表前缀 
+    'DB_PREFIX' => '', // 数据库表前缀
     'DB_CHARSET'=> 'utf8', // 字符集
     'DB_DEBUG'  =>  false, // 数据库调试模式 开启后可以记录SQL日志 3.2.3新增
-	'URL_MODEL' => '3',		
-	'SHOW_PAGE_TRACE' =>false, 
+	'URL_MODEL' => '3',
+	'SHOW_PAGE_TRACE' =>false,
 	'SESSION_AUTO_START' =>true,
 );
